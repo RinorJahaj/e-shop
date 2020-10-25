567607b16407bfc7a6e2335495b1a82ebb4143b2
Rinor <rinorjahaj392@gmail.com>
2020-10-25 22:11:35 +0100

    Backend and Frontend setup"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
-  return <div className="App">app</div>;
+  return (
+    <Router>
+      <Fragment>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Route exact path="/" component={HomeScreen}></Route>
+            <Route path="/product/:id" component={ProductScreen}></Route>
+          </Container>
+        </main>
+        <Footer />
+      </Fragment>
+    </Router>
+  );
 };
 
 export default App;
