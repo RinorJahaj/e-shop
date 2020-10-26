2a7370e7d4114607a2cc07c1ce0bd08f4977cf70
Rinor <rinorjahaj392@gmail.com>
2020-10-26 20:53:08 +0100

    Connected DB

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+const bcryptjs = require("bcryptjs");
+
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: bcryptjs.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "John Doe",
+    email: "john@example.com",
+    password: bcryptjs.hashSync("123456", 10),
+  },
+  {
+    name: "Jane Doe",
+    email: "jane@example.com",
+    password: bcryptjs.hashSync("123456", 10),
+  },
+];
+
+module.exports = users;
